c21f06bbfa691a020db28bb654a3a097760f5f26
Marquis <abahmarquis@gmail.com>
2025-02-23 06:15:25 -0800

    updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,14 +19,7 @@ export default {
     cssPath: "@/assets/css/main.css",
   },
   axios: {
-    // Axios options here
     timeout: 10000, // Example: set timeout to 10 seconds
   },
   plugins: [],
-  // buildModules: [
-  //   '@nuxtjs/moment'
-  // ]
-  // alias: {
-  // 	'@': '/'
-  // },
 };
